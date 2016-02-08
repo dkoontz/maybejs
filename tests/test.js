746fdfd92dd64d724ff79d2e230874523dbb240b
David Koontz <david@koontzfamily.org>
2016-02-08 14:51:14 -0700

    Babeling src before publishing

--- tests/test.js
+++ tests/test.js
@@ -1,28 +1,28 @@
-const Maybe = require('../src/').Maybe;
-const Either = require('../src/').Either;
+const Maybe = require('../build/src/').Maybe;
+const Either = require('../build/src/').Either;
 
-// console.log(Maybe.Just(5));
-// console.log(Maybe.Nothing);
-//
-// console.log(Maybe.Just(5).valueOr(0));
-// console.log(Maybe.Nothing.valueOr(0));
-// console.log(Maybe.Nothing === Maybe.Nothing);
-// console.log(Maybe.Just(8).map(n => n.toString()));
-// console.log(Maybe.Nothing.map(n => n * 2));
-//
-// console.log(Maybe.Just(2).chain(n => {
-//   if (n > 3) return Maybe.Nothing;
-//   else return Maybe.Just(n * 2);
-// }));
-// console.log(Maybe.Nothing.chain(n => n * 2));
+console.log(Maybe.Just(5));
+console.log(Maybe.Nothing);
 
-// console.log(Either.Left(5));
-// console.log(Either.Right(5));
-// console.log(Either.Left(5).isLeft());
-// console.log(Either.Right(5).isLeft());
-//
-// console.log(Either.Right(20).map(n => n - 6));
-// console.log(Either.Left(20).map(n => n - 6));
+console.log(Maybe.Just(5).valueOr(0));
+console.log(Maybe.Nothing.valueOr(0));
+console.log(Maybe.Nothing === Maybe.Nothing);
+console.log(Maybe.Just(8).map(n => n.toString()));
+console.log(Maybe.Nothing.map(n => n * 2));
+
+console.log(Maybe.Just(2).chain(n => {
+  if (n > 3) return Maybe.Nothing;
+  else return Maybe.Just(n * 2);
+}));
+console.log(Maybe.Nothing.chain(n => n * 2));
+
+console.log(Either.Left(5));
+console.log(Either.Right(5));
+console.log(Either.Left(5).isLeft());
+console.log(Either.Right(5).isLeft());
+
+console.log(Either.Right(20).map(n => n - 6));
+console.log(Either.Left(20).map(n => n - 6));
 
 console.log(doStuffToThing(Either.Right(20)));
 console.log(doStuffToThing(Either.Left(20)));
